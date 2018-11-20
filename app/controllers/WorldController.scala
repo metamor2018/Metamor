7fb4bd2a853bc3d804dd8fdaa8184c9d18ce78c2
kounokouichi <[hamuichi9231@gmail.com]>
2018-11-20 16:04:10 +0900

    [update]testCreatorIDをcreatorIdに変更

--- app/controllers/WorldController.scala
+++ app/controllers/WorldController.scala
@@ -29,10 +29,10 @@ class WorldController @Inject()(cc: ControllerComponents)
   def create() = Action(circe.json[WorldForm]) { implicit request =>
     val worldForm = request.body
     //ログインしてる程のID
-    val testCreatorID = "7"
+    val creatorId = "7"
 
     try {
-      worldService.create(worldForm.name, testCreatorID, worldForm.detail, worldForm.startedAt)
+      worldService.create(worldForm.name, creatorId, worldForm.detail, worldForm.startedAt)
       Ok(("status" -> "ok").asJson)
     } catch {
       case e: Exception =>
